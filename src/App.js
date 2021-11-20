fd8fa4e88c90c24968ec158d27f1f03bbc8efc27
YahuiG <zzm0815gyh@gmail.com>
2021-11-19 22:22:54 -0800

    Start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const client_id = "c6f612100e814fe08fdc85c1f6070adb";
+  const fetchUrl = `https://api.spotify.com/v1/search?type=album&include_external=audio`;
+  
+  const fetchAlbums = () => {
+    axios.get(fetchAlbums)
+  }
   return (
     <div className="App">
       <header className="App-header">
