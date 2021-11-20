75ace60adc78388fcdc8ec0ed576a3eb9685b487
YahuiG <zzm0815gyh@gmail.com>
2021-11-20 15:28:54 -0800

    DIsplay

--- src/Accordion.js
+++ src/Accordion.js
@@ -52,58 +52,13 @@ export default function Accordion() {
 
   return (
     <div className="album">
-      <h3>Select An Album:</h3>
-      
-      <div>
-          {image.map((img, index) => (
-              <img src={img.image.url}/>
-          ))}
-      </div>
-      
-      <Panel
-        name="Abbey Road"
-        src={fetchAlbumCover}
-        isActive={activeIndex === 0}
-        onShow={() => setActiveIndex(0)}
-      >
-        <li>Abbey1</li>
-        <li>Abbey2</li>
-        <li>Abbey3</li>
-        <li>Abbey4</li>
-      </Panel>
-      <Panel
-        name="With the Beatles"
-        src="https://upload.wikimedia.org/wikipedia/en/5/51/TheBeatles-LetItBe%282011VinylReissue%29.png"
-        isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(1)}
-      >
-        <li>WTB1</li>
-        <li>WTB2</li>
-        <li>WTB3</li>
-        <li>WTB4</li>
-      </Panel>
-      <Panel
-        name="Let it be"
-        src="https://upload.wikimedia.org/wikipedia/en/5/51/TheBeatles-LetItBe%282011VinylReissue%29.png"
-        isActive={activeIndex === 2}
-        onShow={() => setActiveIndex(2)}
-      >
-        <li>LIB1</li>
-        <li>LIB2</li>
-        <li>LIB3</li>
-        <li>LIB4</li>
-      </Panel>
-      <Panel
-        name="Sgt. Peppers Lonely Hearts Club Band"
-        src="https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg"
-        isActive={activeIndex === 3}
-        onShow={() => setActiveIndex(3)}
-      >
-        <li>SP1</li>
-        <li>SP2</li>
-        <li>SP3</li>
-        <li>SP4</li>
-      </Panel>
+      <h3>Select an Album</h3>
+      {album.map((album, index) => (
+        <div key={index}>
+          <img src={album.images} />
+          <p>{album.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
